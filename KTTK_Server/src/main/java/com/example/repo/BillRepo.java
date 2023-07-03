a5b2a842e5bd82f49192d5e0a6009acda6ac2bde
linh <banhtrangmamruoc000@gmail.com>
2023-07-03 22:58:37 +0700

    xoa folder

--- KTTK_Server/src/main/java/com/example/repo/BillRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Bill;
-
-public interface BillRepo extends JpaRepository<Bill, Integer>{
-
-}
